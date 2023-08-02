2a86a2164fc8147b5edf2d6a6ca93bb30cd05e22
Andrew82106 <qq281512383@163.com>
2023-08-02 11:46:14 +0800

    pack getSimi

--- workspace/generateSimi.py
+++ workspace/generateSimi.py
@@ -1,5 +1,8 @@
 from Utils.loadTmodel import chatglm_ptuing
 
-model = chatglm_ptuing()
 
-print(model.response(textIn="我今晚在酒店吃了一顿牛排")[0])
+def getSimi(TextIn):
+    model = chatglm_ptuing()
+    return model.response(textIn="我今晚在酒店吃了一顿牛排")[0]
+
+print(getSimi("小明h"))
